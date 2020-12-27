6dcd05f93f4da1b4d61d5f28327ee334c514a368
ahmed boghdady <ahmed.bogha2841988@gmail.com>
2020-12-27 09:41:25 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Profile { get; set; } //.net Developer
+        public string Avatar { get; set; } //pic
+        public Address Address { get; set; }
+
+    }
+}
